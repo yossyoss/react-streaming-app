6df34904e6cb4288aea52187bea9efc5042f7771
yossyoss <yosi0679@gmail.com>
2019-01-26 23:32:23 +0200

    adding lodash and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,45 @@
-import {SIGN_IN,SIGN_OUT} from './types'
-export const signIn = (userId) => {
+import streams from "../apis/streams";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
+
+export const signIn = userId => {
   return {
     type: SIGN_IN,
-    payload:userId
+    payload: userId
   };
 };
 
 export const signOut = () => {
-    return {
-      type: SIGN_OUT
-    };
+  return {
+    type: SIGN_OUT
   };
-  
+};
+
+export const createStream = formValues => async dispatch => {
+  const res = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: res.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+export const fetchStream = id => async dispatch => {
+  const res = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+export const editStream = (id, formValues) => async dispatch => {
+  const res = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: res.data });
+};
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
