3a4cafb20c63920c4c24706022b4b95904d87839
yossyoss <yosi0679@gmail.com>
2019-02-04 00:04:49 +0200

    refactoring create component

--- client/src/components/streams/StreamEdit.jsx
+++ client/src/components/streams/StreamEdit.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -11,7 +11,9 @@ class StreamList extends Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       );
